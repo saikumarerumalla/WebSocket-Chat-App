da1864bfd05a032e8e473b968142b555ac5c573f
saikumarerumalla <saikumar.erumalla@gmail.com>
2025-05-30 00:24:02 +0900

    initial commit

---
+++ src/main/java/com/explore/WebSocket_Chat_App/WebSocketChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.explore.WebSocket_Chat_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketChatAppApplication.class, args);
+	}
+
+}
